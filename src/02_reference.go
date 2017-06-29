4d3707200e6516be2971ccbaf21a6fd98a9b04ea
copentop <copentop@gmail.com>
2017-06-29 10:32:24 +0800

    # add

---
+++ src/02_reference.go
@@ -0,0 +1,13 @@
+package main
+
+
+import "fmt"
+import "reflect"
+
+func main() {
+
+	var age = 29
+
+	fmt.Println("age type:", reflect.TypeOf(age))
+
+}
---
+++ src/02_short_err.go
@@ -0,0 +1,13 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+
+	name, age := "cat"
+
+
+	fmt.Println(" name is", name, " age is", age)
+}
